cbb6be2820d03f1b1593d03755482e6fc2008dce
Ron Says <ronsayers@yahoo.com>
2024-09-08 02:15:17 -0400

    Update toggle_block.sh
    added GPL license info

--- toggle_block.sh
+++ toggle_block.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+# 
+# This script is part of a project licensed under the GNU General Public License v3.0.
+# 
+# Copyright (C) RN-Say/toggle_block 2024
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
 
-# Arguments passed from the automation or command line
+# Input arguments
 target_block="$1"
 toggle_state="$2"
 target_file="$3"
